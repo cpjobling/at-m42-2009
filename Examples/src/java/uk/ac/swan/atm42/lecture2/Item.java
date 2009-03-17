c02d3eb1a088c3f55686ba8b00a9e4f5fc8bd7cb
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-17 18:41:59 +0000

    Added first example: uk.ac.swan.egm42.lecture2.Item.java.

---
+++ Examples/src/java/uk/ac/swan/atm42/lecture2/Item.java
@@ -0,0 +1,74 @@
+/**
+ * Lecture 2
+ */
+ 
+package uk.ac.swan.atm42.lecture2;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+	 A found item in an hypothetical adventure game.
+	 Used to illutsrate the differences between Java and Groovy.
+	 
+	 @author Dr Chris P. Jobling
+	 @version 1.0
+*/
+public class Item 
+{
+	private String name;
+	private String description;
+	
+	/**
+	 * Default constructor. Needed to be able to create an Item with no name and no description.
+	 */
+	 public Item() {}
+	 
+	 /**
+		 Constructor.
+		 @param name the name of the item.
+	     @param description a description of the item;
+	 */
+	public Item(String name, String description)
+	{
+			this.name = name;
+			this.description = description;	 	
+	}
+	// name and description are properties and need getter/setter methods
+	public String getName()
+	{
+		return this.name;
+	}	
+
+	public void setName(String name)
+	{
+		this.name = name;		
+	}
+	
+	public String getDescription()
+	{
+		return this.description;
+	}	
+
+	public void setDescription(String description)
+	{
+		this.description = description;		
+	}
+
+	public static void main(String [] args)
+	{
+		List room = new ArrayList();
+		room.add(new Item("sword", "An old and rusty sword."));
+		room.add(new Item("cheese", "A lump of twenty day old cheddar."));
+		room.add(new Item("broken stool", "A three-legged stool with only two legs."));
+		
+		System.out.println("This room contains");
+		for (Iterator iter = room.iterator(); iter.hasNext();)
+		{
+			Item item = (Item) iter.next();
+			System.out.println(item.getName() + ": " + item.getDescription());
+		}		
+			
+	}
+}
