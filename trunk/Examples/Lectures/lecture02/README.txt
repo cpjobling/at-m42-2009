a4ca539b48ab3395d62667cdfd08d710a901cb96
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-27 16:23:57 +0000

    Added examples 2 and 3 to lecture 2.

--- trunk/Examples/Lectures/lecture02/README.txt
+++ trunk/Examples/Lectures/lecture02/README.txt
@@ -1,4 +1,6 @@
 This folder contains all the examples from Lecture 2.
 
   * example1.groovy -- everything is an object
+  * example2.groovy -- relational operators
+  * example3.groovy -- equality operators
 
---
+++ trunk/Examples/Lectures/lecture02/example2.groovy
@@ -0,0 +1,9 @@
+// Relational operators
+def number = -1
+def age = 68
+def limit = 100
+def index = 99
+
+assert number < 0          // Is number negative?
+assert age >= 65           // is this a senior citizen?
+assert index <= limit - 1  // has the limit been reached?
