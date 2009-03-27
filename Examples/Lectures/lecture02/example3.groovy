10cf50d0004d178ec167e896bb6f2eae6b97146b
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-27 16:23:57 +0000

    Added examples 2 and 3 to lecture 2.

---
+++ Examples/Lectures/lecture02/example3.groovy
@@ -0,0 +1,9 @@
+// Equality operators
+def forename = "Chris"
+def surname = "Jobling"
+assert forename == "Chris"
+assert surname != "Joblin"
+
+def age == 23
+def number == 25
+assert age == number
