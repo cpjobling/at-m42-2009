db0fd138dc08078386cb1c1671cfeaa6ee30e5ed
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-28 13:52:18 +0000

    Corrected example5.groovy.

--- Examples/lecture02/example5.groovy
+++ Examples/lecture02/example5.groovy
@@ -1,3 +1,9 @@
 // Basic operations on Strings
 def message = "Hello World!"
-assert 'Hello ' + 'world' == 'Hello world'    // concatenate
+assert 'Hello' + 'world' == 'Helloworld'    // concatenate
+assert 'Hello' * 3 == 'HelloHelloHello'     // repeat
+assert message - 'o World' == 'Hell!'       // remove first occurence
+assert message.size() == 12                 // synonymous with length
+assert message.length() == 12               // synonymous with size
+assert message.count('o') == 2             
+assert message.contains('ell') == true
