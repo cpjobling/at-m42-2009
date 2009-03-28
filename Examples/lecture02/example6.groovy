03e739c5f63278bd7befcf8c17f670678afc1c4b
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-28 13:17:31 +0000

    Completed example 6.

--- Examples/lecture02/example6.groovy
+++ Examples/lecture02/example6.groovy
@@ -1,16 +1,24 @@
 // String methods
 assert 'Hello'.compareToIgnoreCase('hello') == 0
+
 assert 'Hello'.concat(' world') == 'Hello world'
+
 assert 'Hello'.endsWith('lo') == true
+
 assert 'Hello'.equalsIgnoreCase('hello') == true
+
 assert 'Hello'.indexOf('lo') == 3
 assert 'Hello world'.indexOf('o', 6) == 7
+
 assert 'Hello'.matches('Hello') == true
 assert 'Hello'.matches('He') == false
 assert 'Hello'.replaceAll('l','L') == 'HeLLo'
+
 assert 'Hello world'.split('l') == ['He', '',  'o wor', 'd']
+
 assert 'Hello'.substring(1) == 'ello'
 assert 'Hello'.substring(1, 4) == 'ell'
+
 assert 'Hello'.toUpperCase() == 'HELLO'
 
 def message = 'Hello'
@@ -26,4 +34,29 @@ assert message.getAt([0, 2, 4]) == 'Hlo'
 
 def greeting = message.leftShift(' world')  // greeting is a StringBuffer not a String
 assert greeting.toString() == 'Hello world'
-greeting << ' world').toString() == 'Hello world'
+greeting << ' world'
+assert greeting.toString() == 'Hello world world'
+
+assert message.minus('ell') == 'Ho'
+assert message - 'ell' == 'Ho'
+
+assert message.padLeft(4) == 'Hello'
+assert message.padLeft(11) == '      Hello'
+assert message.padLeft(11, '#') == '######Hello'
+
+assert message.padRight(4) == 'Hello'
+assert message.padRight(11) == 'Hello      '
+assert message.padRight(11, '#') == 'Hello######'
+
+assert message.plus(' world') == 'Hello world'
+assert message + ' world' == 'Hello world'
+
+assert message.replaceAll('[a-z]') {ch -> ch.toUpperCase() } == 'HELLO'
+
+assert message.reverse() == 'olleH'
+
+assert message.toList() == ['H', 'e', 'l', 'l', 'o']
+
+message = 'Hello world'
+assert message.tokenize() == ['Hello', 'world']
+assert message.tokenize('l') == ['He', 'o wor', 'd']
