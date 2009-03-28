8fad1e68431bd0f3086c7ae809accd54f1752a2c
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-28 10:26:52 +0000

    Minor rearrangement of folders better for on-line hosting.

--- trunk/Examples/Lectures/lecture02/example3.groovy
+++
@@ -1,9 +0,0 @@
-// Equality operators
-def forename = "Chris"
-def surname = "Jobling"
-assert forename == "Chris"
-assert surname != "Joblin"
-
-def age == 23
-def number == 25
-assert age == number
