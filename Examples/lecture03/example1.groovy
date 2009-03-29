400f8130f5e2ba6ff0250065cd81b911bec68c99
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-29 16:22:10 +0000

    Completed lecture03/example1.groovy.

--- Examples/lecture03/example1.groovy
+++ Examples/lecture03/example1.groovy
@@ -12,3 +12,15 @@ assert [1, 2, 3, 4][2] == 3
 assert numbers [0..2] == [97, 98, 99]
 assert numbers [0..<2] == [97, 98]
 
+// Using index to add new items to a list
+numbers[1] = 2
+assert numbers == [97, 2, 99, 100]
+numbers[2] = [33, 44]
+assert numbers == [97, 2, [33, 44], 100]
+
+// Adding items
+assert (numbers << 101) ==  [97, 2, [33, 44], 100, 101]
+assert numbers + [102, 103] == [97, 2, [33, 44], 100, 101, 102, 103]
+
+// Removing items
+assert numbers - [100] == [97, 2, [33, 44], 101]
