6277738c9806b0c77ee06e6d884c47864b45810d
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-03-30 14:45:00 +0000

    Added a batch file to initialise environment

---
+++ atm42-init.bat
@@ -0,0 +1,17 @@
+REM - Setting up environment AT-M42 Client Server Programming for the Java Plaform
+REM - for Groovy/Grails on Windows
+
+@ECHO OFF
+REM Edit the following lines to match your set up
+SET JAVA_HOME=d:\java\jdk1.6.0_10
+SET GROOVY_HOME=d:\java\languages\Groovy-1.6.0
+SET GRAILS_HOME=d:\java\applications\grails-1.0
+SET AT-M42=e:\dev\at-m42-2009
+
+REM Add JAVA_HOME\bin;GROOBY_HOME\bin;GRAILS_HOME\bin; to PATH
+set PATH=%JAVA_HOME%\bin;%GROOVY_HOME%\bin;%GRAILS_HOME%\bin;%PATH%
+
+cd %AT-M42%\Examples
+
+
+ECHO Try groovyConsole lecture02\example1.groovy
