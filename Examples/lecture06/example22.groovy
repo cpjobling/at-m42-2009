11e56510ef7b1eabb7368bb7f5b0e1c9e2fcda14
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-06 11:02:03 +0000

    Added examples Examples/lecture06/example22.groovy.

---
+++ Examples/lecture06/example22.groovy
@@ -0,0 +1,23 @@
+// WC utility
+
+import java.io.File
+
+
+  // Counters
+def chars = 0
+def words = 0
+def lines = 0
+
+if (args.size() != 1) {
+    println 'Usage: example22.groovy filename'
+} else {
+        // Process the file
+    new File(args[0]).eachLine { line ->
+        chars += 1 + line.length()
+        words += line.tokenize().size()
+        lines++
+    }
+    
+    // Print the outcome
+    println "chars: ${chars}; words: ${words}; lines: ${lines}"
+}
