a071eb2dcee24e8cb4dd0f1f92f8f2248dc97d04
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-08 11:04:19 +0000

    Added example11.groovy.

---
+++ trunk/Examples/lecture07/example11.groovy
@@ -0,0 +1,27 @@
+// Inherited features
+
+class WeightyItem extends Item {
+    
+    String toString() {
+        return 'WeightyItem: ' + super.toString() + "; weight: ${weight}"
+    }
+
+// ---- properties -------------------------
+
+    def weight = 0
+}
+
+  // populate a list with weighty items
+def items = [new WeightyItem(name : 'Magical sword', value : 1000, weight : 10),
+             new WeightyItem(name : 'Cloak of invisibility', value : 500),
+             new Item(name : 'Magic Amulet', value : 700) ]
+
+items.each { item ->
+    println item    // automatically call toString
+}
+
+def wine = new WeightyItem(name : 'A Flagon of Wine', value : 2, weight : 2)
+
+    // use methods and inherited methods
+println "Weight: ${wine.weight}"
+prinln "Name: ${wine.name}"
