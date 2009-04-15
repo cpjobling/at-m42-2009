f7740c9350c70671c1ddaacfacd4ded20f1e99ee
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-15 17:48:51 +0000

    Lecture 9 complete.

---
+++ trunk/Examples/lecture09/ryanAndMonicaJob2.groovy
@@ -0,0 +1,40 @@
+//shared resource problems
+public class RyanAndMonicaJob implements Runnable {
+    // shared bank account
+    def account = new BankAccount(balance : 100);
+
+    public void run() {
+        for (i in 1..<10 ) {
+            this.makeWithdrawal(10);
+            if (account.balance < 0) {
+                System.out.println("Oops ... overdrawn!");
+            }
+        }
+    }
+
+    def synchronized makeWithdrawal(amount) {
+        if (account.balance >= amount) {
+            println Thread.currentThread().getName() + " is about to withdraw $amount"
+            try {
+                println Thread.currentThread().getName() + " is going to sleep"
+                Thread.sleep(500)
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            println Thread.currentThread().getName() + " woke up."
+            account.withdraw(amount);
+            println Thread.currentThread().getName() + " completes the withdrawal"
+        } else {
+            println "Sorry, not enough for " + Thread.currentThread().getName()
+        }
+    }
+}
+
+
+def theJob = new RyanAndMonicaJob();
+def one = new Thread(theJob);
+def two = new Thread(theJob);
+one.name = 'Ryan'
+two.name = 'Monica'
+one.start()
+two.start()
