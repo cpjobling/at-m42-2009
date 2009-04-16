d92f96179bc410ec9fd0a2a9867115562f83cdc0
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-16 19:27:37 +0000

    Check point at end of working day. Lecture 11 incomplete.

--- Examples/lecture11/build.bat
+++ Examples/lecture11/build.bat
@@ -1,3 +1,6 @@
 REM Build RMI example
 
 javac *.java -d . 
+
+rmic -classpath . -d . uk.ac.swan.atm42.rmi.TimeHere
+
--- Examples/lecture11/displayTimeHere.groovy
+++ Examples/lecture11/displayTimeHere.groovy
@@ -1,12 +1,12 @@
 // Uses remote object TimeHere.
 
-package uk.ac.swan.atm42.rmi;
+package uk.ac.swan.atm42.rmi
 
-import java.rmi.Naming;
-import java.rmi.RMISecurityManager;
+import java.rmi.Naming
+import java.rmi.RMISecurityManager
 
-System.setSecurityManager(new RMISecurityManager());
-def t = (TimeHereI) Naming.lookup("//dell-laptop:2005/TimeHere");
+System.setSecurityManager(new RMISecurityManager())
+def t =  Naming.lookup("//dell-laptop:2005/TimeHere")
 for (i in 0..< 10) {
 	println "Time on remote server: ${t.getTimeHere()}"
 } ///:~
