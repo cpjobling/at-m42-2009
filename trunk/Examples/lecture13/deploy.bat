a0f84326f564b21e4beaca916d930918d2af2eb4
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-18 09:54:11 +0000

    Renamed hello.groovy to helloGroovlet.groovy. Added compile servlets to deploy.bat.

--- trunk/Examples/lecture13/deploy.bat
+++ trunk/Examples/lecture13/deploy.bat
@@ -4,6 +4,10 @@ REM - You will need to check values for TOMCAT_HOME and GROOVY_HOME before runni
 set TOMCAT_HOME="D:\java\servers\Apache Tomcat 6.0.18"
 set GROOVY_HOME=D:\java\languages\Groovy-1.6.0
 
+REM compile servlets
+javac web\HelloServlet.java -cp %TOMCAT_HOME%\lib\servlet-api.jar -d web\WEB-INF\classes
+
+
 mkdir %TOMCAT_HOME%\weapps\at-m42-examples
 robocopy /e web  %TOMCAT_HOME%\webapps\at-m42-examples
 
