b205fe5f13c9c4ed0042eb21de5402d5c3fc507e
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-25 10:52:58 +0000

    At end of lecture 14.

--- Examples/lecture14/TimeHereClient.java
+++ Examples/lecture14/TimeHereClient.java
@@ -1,18 +1,18 @@
 // Client program for TimeHereBean
 
 public class TimeHereClient {
-	  public static void main(String[] args) throws Exception {
-	  	    // Get a JNDI context using
-	  	    // the JNDI Naming service:
-	  	    javax.naming.Context context = new
-	  	  	javax.naming.InitialContext();
-	  	  	// Look up the home interface in the JNDI Naming service:
-	  	  	Object ref = context.lookup("TimeHere");
-	  	  	// Cast the remote object to the home interface:
-	  	  	TimeHereHome home = (TimeHereHome)  javax.rmi.PortableRemoteObject.narrow(ref, TimeHereHome.class);
-            // Create a remote object from the home interface:
-            TimeHere pt = home.create();
-            // Invoke getTimeHere()
-            System.out.println("Perfect Time EJB invoked, time is: " + pt.getTimeHere());
-	}
+    public static void main(String[] args) throws Exception {
+        // Get a JNDI context using
+        // the JNDI Naming service:
+        javax.naming.Context context = new
+        javax.naming.InitialContext();
+        // Look up the home interface in the JNDI Naming service:
+        Object ref = context.lookup("TimeHere");
+        // Cast the remote object to the home interface:
+        TimeHereHome home = (TimeHereHome)  javax.rmi.PortableRemoteObject.narrow(ref, TimeHereHome.class);
+        // Create a remote object from the home interface:
+        TimeHere pt = home.create();
+        // Invoke getTimeHere()
+        System.out.println("Perfect Time EJB invoked, time is: " + pt.getTimeHere());
+    }
 } 
