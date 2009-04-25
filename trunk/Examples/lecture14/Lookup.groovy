26589a978c1996f6ad02e0a0132321fecf857d0b
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-25 15:02:53 +0000

    Added Lookup.groovy and latest wiki text.

---
+++ trunk/Examples/lecture14/Lookup.groovy
@@ -0,0 +1,21 @@
+import groovy.sql.*
+
+def DB = 'jdbc:derby:CDs'
+def USER = ''
+def PASSWORD = ''
+def DRIVER = 'org.apache.derby.jdbc.EmbeddedDriver'		
+
+    // Connect to database
+def sql = Sql.newInstance(DB, USER, PASSWORD. DRIVER)
+
+def QUERY = """
+SELECT albums.title, bands.band_name, albums.year
+FROM bands INNER JOIN albums ON
+bands.band_id = albums.artist
+WHERE (((albums.category)='${args[0]}'))
+"""
+
+sql.eachRow(QUERY) { r ->
+  println "Have a result!"
+  println "${r.title"}  by ${r.band_name} released in ${r.year}"
+}
