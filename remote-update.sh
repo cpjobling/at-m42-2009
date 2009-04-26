6b7857610a9290ea8b0fce9d3d3c7ad5e9ca645c
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-26 15:39:09 +0000

    Corrected remote-update.sh

--- remote-update.sh
+++ remote-update.sh
@@ -5,5 +5,5 @@ svn update
 echo "Exporting code distribution..."
 svn export . at-m42-source
 echo "Zipping ..."
-zip -m -r at-m42-source.zip at-m42-source
+zip -q -m -r at-m42-source.zip at-m42-source
 echo "Done..."
