a4c35f1a070ed7462f5e86cb3581556176172b35
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-26 19:18:31 +0000

    Ready (as it will ever be) for day 1!

--- trunk/atm42-init.bat
+++ trunk/atm42-init.bat
@@ -1,11 +1,11 @@
-REM - Setting up environment AT-M42 Client Server Programming for the Java Plaform
+REM - Setting up environment AT-M42 Client Server Programming for the Java Platform
 REM - for Groovy/Grails on Windows
 
 @ECHO OFF
 REM Edit the following lines to match your set up
 SET JAVA_HOME=d:\java\jdk1.6.0_13
-SET GROOVY_HOME=d:\java\languages\Groovy-1.6.0
-SET GRAILS_HOME=d:\java\applications\grails-1.0
+SET GROOVY_HOME=d:\java\languages\Groovy-1.6.2
+SET GRAILS_HOME=d:\java\applications\grails-1.1
 SET DERBY_HOME=d:\java\applications\db-derby-10.4.2.0-bin
 
 
@@ -19,4 +19,4 @@ cd %AT-M42%\Examples
 %DERBY_HOME%\bin\setEmbeddedCP
 
 
-ECHO Try groovyConsole lecture02\example1.groovy
+ECHO "Try groovyConsole lecture02\example1.groovy"
