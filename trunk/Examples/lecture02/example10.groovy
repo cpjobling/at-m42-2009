d69ea2167213f25d9ababaad8c8745503a44a890
cpjobling <cpjobling@f8ffffac-0f3e-11de-b8e2-c32c05206e41>
2009-04-27 13:49:17 +0000

    Corrected during lecture

--- trunk/Examples/lecture02/example10.groovy
+++ trunk/Examples/lecture02/example10.groovy
@@ -2,6 +2,6 @@
 assert 'aaaaab' =~ /a*b/     // zero or more 'a' followed by 'b'
 assert 'b' =~ /a*b/          // zero 'a' still matches!
 assert 'aaacd' =~ /a*c?d/    // zero or more 'a', optional 'c' followed by 'b'
-assert 'aaacd' =~ /a*c?d/    // the 'c' is optional
+assert 'aaad' =~ /a*c?d/    // the 'c' is optional
 assert 'aaaaaab' =~ /a{5}b/  // five 'a' followed by 'b'
 assert ! ('aab' =~ /a{5}b/)  // there aren't 5 'a'
